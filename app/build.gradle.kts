734dcf94cdb8e8158c3e86bdfa31f4cbcb0db6fe
sinai <sinailei@gmail.com>
2024-01-07 00:30:50 +0200

    Finished login and signup page

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.myproject1"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myproject1"
--- app/src/main/java/com/example/myproject1/MainActivity.java
+++ app/src/main/java/com/example/myproject1/MainActivity.java
@@ -1,14 +1,59 @@
 package com.example.myproject1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager2 viewPager2;
+    private ViewPagerAdapter viewPagerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.tabLayout = findViewById(R.id.tab_layout);
+        this.viewPager2 = findViewById(R.id.view_pager);
+
+        this.tabLayout.addTab(this.tabLayout.newTab().setText("log In"));
+        this.tabLayout.addTab(this.tabLayout.newTab().setText("Sign Up"));
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        this.viewPagerAdapter = new ViewPagerAdapter(fragmentManager, getLifecycle());
+        this.viewPager2.setAdapter(this.viewPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab)
+            {
+                // TODO: find out why I can't write this. in listeners I suppose?
+                viewPager2.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+
+        this.viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position)
+            {
+                tabLayout.selectTab(tabLayout.getTabAt(position));
+            }
+        });
     }
 }
