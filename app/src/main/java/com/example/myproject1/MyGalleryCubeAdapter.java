11c57886043c149c5ee1ae12013f1825d8ca020b
HULYO <hulibr12@gmail.com>
2024-06-18 16:27:43 +0300

    commit 18/6 16:30 Made MyGallery completely!

--- app/src/main/java/com/example/myproject1/MyGalleryCubeAdapter.java
+++ app/src/main/java/com/example/myproject1/MyGalleryCubeAdapter.java
@@ -1,6 +1,118 @@
 package com.example.myproject1;
 
 
-public class MyGalleryCubeAdapter {
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+
+import java.util.List;
+
+public class MyGalleryCubeAdapter extends ArrayAdapter<MyGalleryCube>
+{
+
+
+//    TextView tvItemSubject;
+//    ImageView ivItemDrawing;
+
+    public MyGalleryCubeAdapter(@NonNull Context context, @NonNull List<MyGalleryCube> MyGalleryCubeArrayList) {
+        super(context, 0, MyGalleryCubeArrayList);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        MyGalleryCube myGalleryCube = getItem(position);
+        return myGalleryCube != null ? myGalleryCube.getBitmap().hashCode() : position;
+    }
+
+    @Override
+    public boolean hasStableIds() {
+        return true;
+    }
+
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @Nullable ViewGroup parent)
+    {
+        ViewHolder viewHolder;
+
+        if (convertView == null)
+        {
+            // Layout Inflater inflates each item to be displayed in GridView.
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.custom_gridview_mygallery, parent, false);
+            viewHolder = new ViewHolder();
+            viewHolder.tvItemSubject = convertView.findViewById(R.id.tv_gvItemSubject);
+            viewHolder.ivItemDrawing = convertView.findViewById(R.id.iv_gvItemDrawing);
+            convertView.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        MyGalleryCube myGalleryCube = getItem(position);
+
+        if (myGalleryCube != null)
+        {
+            viewHolder.tvItemSubject.setText(myGalleryCube.getSubject());
+            // Clear previous image to avoid reusing old views with incorrect images
+            viewHolder.ivItemDrawing.setImageBitmap(null);
+            // Before starting the image loading, set a placeholder
+            viewHolder.ivItemDrawing.setImageResource(R.drawable.mainscreen_mygallery_loading_pic);
+
+
+            // Log the subject and bitmap string to debug
+            Log.d("MyGalleryCubeAdapter", "Loading image for subject: " + myGalleryCube.getSubject() + " with bitmap ID: " + myGalleryCube.getBitmap());
+
+            // get the image from the Firebase Storage
+            FirebaseStorage firebaseStorage= FirebaseStorage.getInstance();
+            StorageReference storageRef = firebaseStorage.getReference();
+            StorageReference imageRef = storageRef.child(myGalleryCube.getBitmap()+".png");
+            imageRef.getBytes(Long.MAX_VALUE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                @Override
+                public void onSuccess(byte[] bytes) {
+                    // Use the bytes to display the image
+                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                    viewHolder.ivItemDrawing.setImageBitmap(bitmap);
+                    Log.d("MyGalleryCubeAdapter", "Image loaded successfully for subject: " + myGalleryCube.getSubject());
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception exception)
+                {
+                    Log.d("MyGalleryCubeAdapter", "Failed to load image for subject: " + myGalleryCube.getSubject() + ". Error: " + exception.getMessage());
+                }
+            });
+        }
+
+        return convertView;
+    }
+
+
+
+
+
+    private static class ViewHolder {
+        TextView tvItemSubject;
+        ImageView ivItemDrawing;
+    }
+
+
 
 }
+
--- app/src/main/java/com/example/myproject1/MyGalleryFragment.java
+++ app/src/main/java/com/example/myproject1/MyGalleryFragment.java
@@ -2,11 +2,27 @@ package com.example.myproject1;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +39,15 @@ public class MyGalleryFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+
+
+    View fragView;
+    GridView gvMyGallery;
+    User user;
+    ArrayList<MyGalleryCube> myGalleryCubes;
+
+
 
     public MyGalleryFragment() {
         // Required empty public constructor
@@ -46,6 +71,8 @@ public class MyGalleryFragment extends Fragment {
         return fragment;
     }
 
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +80,101 @@ public class MyGalleryFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_gallery, container, false);
+        fragView = inflater.inflate(R.layout.fragment_my_gallery, container, false);
+        return fragView;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (currentUser != null)
+        {
+            String userId = currentUser.getUid();
+            FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+            DocumentReference userRef = firestore.collection("Users").document(userId);
+            userRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot value) {
+                    if (value.exists()) {
+                        user = value.toObject(User.class);
+                        displayGridView(user);
+                    } else {
+                        Log.d("MyGalleryFragment", "User document does not exist");
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e("MyGalleryFragment", "Error fetching user document: " + e.getMessage());
+                }
+            });
+        }
+        else
+        {
+            Log.e("MyGalleryFragment", "No user logged in");
+        }
+
+    }
+
+
+    public void displayGridView(User user)
+    {
+        myGalleryCubes = new ArrayList<>();
+
+        ArrayList<String> gameIdsList = user.getGameIdsList();
+        String username = HomeFragment.username;
+
+        for (int i = 0; i < gameIdsList.size(); i++)
+        {
+            String gameId = gameIdsList.get(i);
+            getSubjectInGameRoom(gameId, username, gameIdsList.size());
+            // Inside the func, I got the subject and then went to continue func
+            // Inside continue func, I added the myGalleryCube with this subject, gameId and username4
+            // Now, all myGalleryCubes are in the arrayList myGalleryCubes.
+        }
+
+    }
+
+    private void getSubjectInGameRoom(String gameId, String username, int gameIdsListSize)
+    {
+        FirebaseFirestore firestore = FirebaseFirestore.getInstance();
+        DocumentReference gameRoomRef = firestore.collection("Games").document(gameId);
+        gameRoomRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+        @Override
+        public void onSuccess(DocumentSnapshot value) {
+            if (value.exists()) {
+                GameRoom gr = value.toObject(GameRoom.class);
+                String grSubject = gr.getSubject();
+                continueDisplayGridView(grSubject, gameId, username, gameIdsListSize);
+            }
+            else
+            {
+                Log.d("MyGalleryFragment", "GameRoom document does not exist");
+            }
+        }
+        });
+    }
+
+
+    private void continueDisplayGridView(String subject, String gameId, String username, int gameIdsListSize)
+    {
+        MyGalleryCube myGalleryCube = new MyGalleryCube(username + gameId, subject);
+        myGalleryCubes.add(myGalleryCube);
+        if (gameIdsListSize == myGalleryCubes.size())
+        {
+            gvMyGallery = fragView.findViewById(R.id.gv_myGallery);
+            MyGalleryCubeAdapter gvAdapter = new MyGalleryCubeAdapter(getContext(), myGalleryCubes);
+            gvMyGallery.setAdapter(gvAdapter);
+        }
+    }
+
 }
